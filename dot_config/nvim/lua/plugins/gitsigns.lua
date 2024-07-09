6ee574a65327cd73c8b3ef54a3950fdef770ac9a
mecattaf <thomasmecattaf@gmail.com>
2024-07-09 17:43:36 +0200

    Fix nvim gitsigns

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -6,11 +6,11 @@ end
 
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr" },
-    change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr" },
-    delete = { hl = "GitSignsDelete", text = "│", numhl = "GitSignsDeleteNr" },
-    topdelete = { hl = "GitSignsTopdelete", text = "│", numhl = "GitSignsTopdeleteNr" },
-    changedelete = { hl = "GitSignsChangedelete", text = "│", numhl = "GitSignsChangedeleteNr" },
-    untracked = { hl = "GitSignsUntracked", text = "│", numhl = "GitSignsUntrackedNr" },
+    add = { text = "│" },
+    change = { text = "│" },
+    delete = { text = "󰍵" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "│" },
   },
 }
