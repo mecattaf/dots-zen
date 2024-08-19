ef7bf2875b55ee36b0a76b0ed607087daf8f2248
mecattaf <thomasmecattaf@gmail.com>
2024-08-19 09:39:04 +0200

    using nvim scrollback buffer in kitty terminal

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -16,6 +16,18 @@ g.mapleader = " "
 
 require("lazy").setup({
   {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+    event = { 'User KittyScrollbackLaunch' },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require('kitty-scrollback').setup()
+    end,
+  },
+  {
   	"robitx/gp.nvim",
   	config = function()
   		require("gp").setup()
